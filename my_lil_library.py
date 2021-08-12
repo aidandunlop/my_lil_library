db357fbe5e1cedc987141c625c57d11c53fb0c84
Alex Georgousis <alexgeo2395@gmail.com>
2021-08-12 11:10:35 +0100

    update to single python file

---
+++ my_lil_library.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def function_one():
+    print("hi from function one")
+    return "a string"
+
+
+def function_two():
+    print("hi from function two")
+    return "another string"
+
+
+def function_three():
+    a_variable = np.float("0.3")
+    return a_variable
